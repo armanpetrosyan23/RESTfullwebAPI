6628ba7d10c5959530c0ae4cd1235a6fe534288d
Arman Petrosyan <armanpetrosyan181@gmail.com>
2018-04-24 21:28:40 +0400

    Add project files.

---
+++ WebAPIFreeman/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPIFreeman.Models;
+
+namespace WebAPIFreeman.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository { get; set; }
+        public HomeController(IRepository rep)
+        {
+            repository = rep;
+        }
+        public ViewResult Index()
+        {
+            return View(repository.Reservations);
+        }
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+
+        }
+    }
+}
---
+++ WebAPIFreeman/Controllers/ReservationController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIFreeman.Models;
+
+namespace WebAPIFreeman.Controllers
+{
+   
+    [Route("api/Reservation")]
+    public class ReservationController : Controller
+    { 
+        private IRepository repository;
+        public ReservationController(IRepository repo)
+        {
+            repository = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get()
+        {
+            return repository.Reservations;
+        }
+
+        [HttpGet("{id}")]
+        public Reservation Get(int id)
+        {
+            return repository[id];
+        }
+
+        
+        [HttpPost]
+        public Reservation Post([FromBody] Reservation res)
+        {
+            return repository.AddReservation(new Reservation
+            {
+                ClientName = res.ClientName,
+                Location = res.Location
+            });
+        }
+
+        [HttpPut]
+        public Reservation Put([FromBody] Reservation res)
+        {
+            return repository.UpdateReservation(res);
+        }
+
+        [HttpPatch("{id}")]
+        public StatusCodeResult Patch(int id, [FromBody]JsonPatchDocument<Reservation> patch)
+        {
+            Reservation res = Get(id);
+            if (res != null)
+            {
+                patch.ApplyTo(res);
+                return Ok();
+            }
+            return NotFound();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            repository.DeleteReservation(id);
+        }
+    }
+}
